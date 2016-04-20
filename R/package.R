5ec6fc7da09c7e0df31545d504b8cc10099adb07
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-20 12:49:34 +0100

    First implementation

--- R/package.R
+++ R/package.R
@@ -1,9 +1,80 @@
 
 #' Match Regular Expressions with a Nicer 'API'
 #'
-#' A small wrapper on 'regexpr' to extract the matches and captured groups from
-  the match of a regular expression to a character vector.
+#' A small wrapper on 'regexpr' to extract the matches and captured
+#' groups from the match of a regular expression to a character vector.
+#' See \code{\link{re_match}}.
 #'
 #' @docType package
 #' @name rematch
 NULL
+
+#' Match a regular expression to a character vector
+#'
+#' This function is a small wrapper on the \code{\link[base]{regexpr}}
+#' base R function, to provide an API that is easier to use.
+#'
+#' Currently only the first occurence of the pattern is used.
+#'
+#' @param pattern Regular expression, defaults to be a PCRE
+#'   expression. See \code{\link[base]{regex}} for more about
+#'   regular expressions.
+#' @param text Character vector.
+#' @param perl Logical, should Perl-compatible regular expessions
+#'   be used?
+#' @param ... Additional arguments to pass to
+#'   \code{\link[base]{regexpr}}.
+#' @return A character matrix of the matched (sub)strings.
+#'   The first column is always the full match. This column is
+#'   named \code{.match}. The result of the columns are capture groups,
+#'   with appropriate column names, if the groups are named.
+#'
+#' @export
+#' @examples
+#' dates <- c("2016-04-20", "1977-08-08", "not a date", "2016",
+#'   "76-03-02", "2012-06-30", "2015-01-21 19:58")
+#' ansidate <- "([0-9]{4})-([0-1][0-9])-([0-3][0-9])"
+#' re_match(text = dates, pattern = ansidate)
+#'
+#' # The same with named groups
+#' ansidaten <- "(?<year>[0-9]{4})-(?<month>[0-1][0-9])-(?<day>[0-3][0-9])"
+#' re_match(text = dates, pattern = ansidaten)
+
+re_match <- function(pattern, text, perl = TRUE, ...) {
+
+  stopifnot(is.character(pattern), length(pattern) == 1, !is.na(pattern))
+  text <- as.character(text)
+
+  match <- regexpr(pattern, text, perl = perl, ...)
+
+  ## Full matches
+  res <- cbind(as.character(
+    ifelse(
+      match == -1,
+      NA_character_,
+      substr(text, match, match + attr(match, "match.length") - 1)
+    )
+  ))
+
+  if (!is.null(attr(match, "capture.start"))) {
+
+    res <- cbind(
+      res,
+      vapply(
+        seq_len(NCOL(attr(match, "capture.start"))),
+        function(i) {
+          start <- attr(match, "capture.start")[,i]
+          len <- attr(match, "capture.length")[,i]
+          end <- start + len - 1
+          res <- substr(text, start, end)
+          res[ start == -1 ] <- NA_character_
+          res
+        },
+        character(length(match))
+      )
+    )
+  }
+
+  colnames(res) <- c(".match", attr(match, "capture.names"))
+  res
+}
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,8 +1,65 @@
 
 context("rematch")
 
-test_that("rematch works", {
+test_that("corner cases", {
 
-  expect_true(TRUE)
+  res <- re_match("", c("foo", "bar"))
+  expect_equal(res, cbind(.match = c("", "")))
+
+  res <- re_match("", c("foo", "", "bar"))
+  expect_equal(res, cbind(.match = c("", "", "")))
+
+  res <- re_match("", character())
+  expect_equal(res, cbind(.match = character()))
+
+  res <- re_match("foo", character())
+  expect_equal(res, cbind(.match = character()))
+
+  res <- re_match("foo (g1) (g2)", character())
+  expect_equal(res, cbind(.match = character(), character(), character()))
+
+  res <- re_match("foo (g1) (?<name>g2)", character())
+  expect_equal(
+    res,
+    cbind(.match = character(), character(), name = character())
+  )
+
+  res <- re_match("foo", "not")
+  expect_equal(res, cbind(.match = NA_character_))
+})
+
+
+test_that("not so corner cases", {
+
+  dates <- c("2016-04-20", "1977-08-08", "not a date", "2016",
+             "76-03-02", "2012-06-30", "2015-01-21 19:58")
+  ansidate <- "([0-9]{4})-([0-1][0-9])-([0-3][0-9])"
+  expect_equal(
+    re_match(text = dates, pattern = ansidate),
+    cbind(
+      .match = c(dates[1:2], NA, NA, NA, "2012-06-30", "2015-01-21"),
+      c("2016", "1977", NA, NA, NA, "2012", "2015"),
+      c("04", "08", NA, NA, NA, "06", "01"),
+      c("20", "08", NA, NA, NA, "30", "21")
+    )
+  )
+
+  ansidaten <- "(?<year>[0-9]{4})-(?<month>[0-1][0-9])-(?<day>[0-3][0-9])"
+  expect_equal(
+    re_match(text = dates, pattern = ansidaten),
+    cbind(
+      .match = c(dates[1:2], NA, NA, NA, "2012-06-30", "2015-01-21"),
+      year = c("2016", "1977", NA, NA, NA, "2012", "2015"),
+      month = c("04", "08", NA, NA, NA, "06", "01"),
+      day = c("20", "08", NA, NA, NA, "30", "21")
+    )
+  )
+})
+
+
+test_that("UTF8", {
+
+  res <- re_match("Gábor", c("Gábor Csárdi"))
+  expect_equal(res, cbind(.match = "Gábor"))
 
 })
