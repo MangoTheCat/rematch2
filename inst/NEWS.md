3341b790ae3be8f226c9c87a4486752e9fc8741c
Gabor Csardi <csardi.gabor@gmail.com>
2016-04-20 20:47:37 +0100

    Make it work when testthat is not available

--- inst/NEWS.md
+++ inst/NEWS.md
@@ -1,4 +1,8 @@
 
+# 1.0.1
+
+* Make `R CMD check` work when `testthat` is not available.
+
 # 1.0.0
 
 First public release.
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
-library(testthat)
-library(rematch)
 
-test_check("rematch")
+if (require(testthat)) {
+  library(rematch)
+  test_check("rematch")
+}
